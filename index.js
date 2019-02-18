a07d883a9f50c034bfaf9946425853ef4c574eaf
BrotherFatcake <BrotherFatcake@brownscastle.com>
2019-02-18 17:29:17 -0600

    init commit

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+function openNav() {
+    document.getElementById('navMenu').setAttribute('class', 'navMenu navMenu a navMenu a:hover navMenuOpen')
+}
+
+function closeNav() {
+    document.getElementById('navMenu').setAttribute('class', 'navMenu navMenu a navMenu a:hover')
+}
